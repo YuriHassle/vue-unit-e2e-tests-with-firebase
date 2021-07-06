3d95eb1455754b5f062a0516537d51211d994281
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-06 00:59:32 -0400

    refactor: remove vuex import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import router from "./router";
-import store from "./store";
+//import store from "./store";
 import firebase from "firebase/app";
 import axios from "axios";
 
@@ -28,7 +28,7 @@ firebase.auth().onAuthStateChanged((user) => {
   if (!app) {
     app = new Vue({
       router,
-      store,
+      //store,
       render: (h) => h(App),
     }).$mount("#app");
   }
